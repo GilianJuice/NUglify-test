3f29adeae7780a0f27b40c1053a94b628c21a5ad
GilianJuice <gilian@juicebv.nl>
2020-09-15 15:25:47 +0200

    READY - Initial commit

---
+++ NUglify test/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using NUglify;
+
+namespace NUglify_test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                var test = File.ReadAllText(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "test.js"));
+                Console.WriteLine($"Input: {test}");
+
+                var result = Uglify.Js(test);
+                Console.WriteLine($"Output: {result}");
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
